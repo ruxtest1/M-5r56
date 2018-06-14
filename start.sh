791eb0e092fce5665173060d717032749aceb233
chukkapun.r <chukkapun.r@gmail.com>
2018-06-14 15:25:07 +0700

    add sh

--- start.sh
+++ start.sh
@@ -12,12 +12,4 @@ git pull
 #pm2 start server/server.js --name $NAME
 pm2 start ecosystem.config.json --env=product
 
-
-#gulp set config
-#node index.js
-
-#pm2 start --name lusy2_app npm -- start
-
-#pm2 start process.json --env development
-
-#ping facebook.com
+ping facebook.com
